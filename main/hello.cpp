58b4fba322b8d4e29e41ba5b4df3e59ff11443e9
Rishav Sinha <rishavsinha61@gmail.com>
2025-03-25 15:12:53 +0530

    Doing this one again

--- main/hello.cpp
+++ main/hello.cpp
@@ -7,6 +7,7 @@ int main(void) {
   cout << "Hello, Jenkins\n";
 
   cout <<" My name is Rishav Sinha and I am having fun doing CC lab";
+  cout <<"I am trying to do this again"<< endl;
   cout << "I have successfully built and run this  \n";
   return 0;
 }
